b2cfcfb2e3f46ac4f48c001b1e1ee6b6030bc160
cocoytech <i.am.cocoytech+github@gmail.com>
2019-07-28 23:43:20 +0800

    Add CRUD repository pattern in the domain

--- TitleMicroservice0000Template.Domain/UseCase/V1/ISqlDataAccess.cs
+++ TitleMicroservice0000Template.Domain/UseCase/V1/ISqlDataAccess.cs
@@ -6,9 +6,9 @@ namespace TitleMicroservice0000Template.Domain.UseCase.V1
 {
     interface ISqlDataAccess
     {
-        List<T> GetAllData<T>(T model, string sql);
+        List<T> GetAllData<T>(T model, string sql, string databaseConnection);
         List<T> GetDataBy<T>(T model, string sql);
-        void InsertData<T>(T model, string sql);
+        void InsertData<T>(T model, string sql, string databaseConnection);
         void UpdateData<T>(T model, string sql);
         void DeleteData<T>(T model, string sql);
     }
--- TitleMicroservice0000Template.Domain/UseCase/V1/SqlDataAccess.cs
+++ TitleMicroservice0000Template.Domain/UseCase/V1/SqlDataAccess.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 
 namespace TitleMicroservice0000Template.Domain.UseCase.V1
@@ -11,9 +15,13 @@ namespace TitleMicroservice0000Template.Domain.UseCase.V1
             throw new NotImplementedException();
         }
 
-        public List<T> GetAllData<T>(T model, string sql)
+        public List<T> GetAllData<T>(T model, string sql, string databaseConnection)
         {
-            throw new NotImplementedException();
+            using (IDbConnection connectionString = new SqlConnection(databaseConnection))
+            {
+                var output = connectionString.Query<T>(sql, new DynamicParameters());
+                return output.ToList();
+            }
         }
 
         public List<T> GetDataBy<T>(T model, string sql)
@@ -21,10 +29,14 @@ namespace TitleMicroservice0000Template.Domain.UseCase.V1
             throw new NotImplementedException();
         }
 
-        public void InsertData<T>(T model, string sql)
+        public void InsertData<T>(T model, string sql, string databaseConnection)
         {
-            throw new NotImplementedException();
+            using (IDbConnection connectionString = new SqlConnection(databaseConnection))
+            {
+                connectionString.Execute(sql, model);
+            }
         }
+
 
         public void UpdateData<T>(T model, string sql)
         {
