b784217a8efaefc4fbca4497257abe76fecdd50e
cocoytech <i.am.cocoytech+github@gmail.com>
2019-07-28 15:29:02 +0800

    Modify the unit test template generated

---
+++ Microservice00001TemplateTest/Logic/V1ActivityUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Microservice00001TemplateTest.Logic
+{
+    public class V1ActivityUnitTest
+    {
+        [Fact]
+        public void AcitivtyPosting()
+        {
+
+        }
+    }
+}
